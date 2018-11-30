3197704184d9dad65c6954686e0ebdafd1b0b9fd
CugeDe <cugede.0x07ffff@gmail.com>
2018-11-30 12:14:18 +0000

    [DEV] Publish InSaleAuthenticationMethod in super mod

--- src/insale/mod.rs
+++ src/insale/mod.rs
@@ -2,4 +2,4 @@ pub mod error;
 pub mod insale_client;
 
 pub use self::error::{Error, ErrorKind};
-pub use self::insale_client::InSaleAPIClient;
+pub use self::insale_client::{InSaleAuthenticationMethod, InSaleAPIClient};
